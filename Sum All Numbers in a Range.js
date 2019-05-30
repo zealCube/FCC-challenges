2869134cf6885ec2ca31ee0816784a3784706c5f
Cube <jsochan@protonmail.com>
2019-05-30 22:07:47 +0200

    set-up

--- Sum All Numbers in a Range.js
+++ Sum All Numbers in a Range.js
@@ -1,3 +1,14 @@
 //We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
 //The lowest number will not always come first.
 
+function sumAll(arr) {
+    return 1;
+  }
+  
+  sumAll([1, 4]);
+
+
+//sumAll([1, 4]) should return 10.
+//sumAll([4, 1]) should return 10.
+//sumAll([5, 10]) should return 45.
+//sumAll([10, 5]) should return 45.
