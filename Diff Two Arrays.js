d3fb524ed3515e65bb3e4563a933dd933472afc6
Cube <jsochan@protonmail.com>
2019-06-01 15:55:51 +0200

    solution

--- Diff Two Arrays.js
+++ Diff Two Arrays.js
@@ -2,9 +2,14 @@
 // In other words, return the symmetric difference of the two arrays.
 
 function diffArray(arr1, arr2) {
-    var newArr = [];
-    
-    return newArr;
-  }
-  
-  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+    return arr1
+    .concat(arr2)
+    .filter(
+        item => !arr1.includes(item) || !arr2.includes(item)
+    )
+}
+
+    //filter = retunrs new array with items that fullfil the condition 
+
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
