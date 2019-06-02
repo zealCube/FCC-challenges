f19485d9c184bc6c9bb0f883e04a1cf8ae587aa4
Cube <jsochan@protonmail.com>
2019-06-02 11:53:48 +0200

    beut

--- Wherefore art thou.js
+++ Wherefore art thou.js
@@ -4,15 +4,25 @@
 function whatIsInAName(collection, source) {
     var keys = Object.keys(source);
 
-    return collection.filter(function(x) {
-      for (let key of keys) {
-        if (!x.hasOwnProperty(key) || x[key] !== source[key]) {
-          return false;
+    return collection.filter(function (x) {
+        for (let key of keys) {
+            if (!x.hasOwnProperty(key) || x[key] !== source[key]) {
+                return false;
+            }
         }
-      }
-      return true;
+        return true;
     });
 }
 
-  console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", 
-  last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+console.log(whatIsInAName([{
+    first: "Romeo",
+    last: "Montague"
+}, {
+    first: "Mercutio",
+    last: null
+}, {
+    first: "Tybalt",
+    last: "Capulet"
+}], {
+    last: "Capulet"
+}));
