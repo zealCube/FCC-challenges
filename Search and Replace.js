fd1e35485fcc55e8d0ec25feeae2957d0ad2626d
Cube <jsochan@protonmail.com>
2019-06-03 21:49:19 +0200

    init

---
+++ Search and Replace.js
@@ -0,0 +1,11 @@
+//Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+//First argument is the sentence to perform the search and replace on.
+//Second argument is the word that you will be replacing (before).
+//Third argument is what you will be replacing the second argument with (after).
+//Note: Preserve the case of the first character in the original word.
+
+function myReplace(str, before, after) {
+    return str;
+  }
+  
+  myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
