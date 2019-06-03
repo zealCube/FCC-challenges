6d870f19140885a65e553e3c2949d6156d60d2e1
Cube <jsochan@protonmail.com>
2019-06-03 21:46:12 +0200

    solution + update

--- Pig Latin.js
+++ Pig Latin.js
@@ -3,8 +3,13 @@
 //If a word begins with a vowel you just add "way" to the end.
 
 function translatePigLatin(str) {
-    
-    return str;
+    if (str.match(/^[aeiou]/)) {
+        return str + "way";
+    } else {
+        return str.replace(/(^[^aeiou]+)/g, "").concat(str.match(/^[^aeiou]+/g) + "ay");
+    }
 }
-
-translatePigLatin("consonant");
+// I would personally consider "y" a vowel here and include it in the RegExp but FCC fails
+// last challenge "should handle words without vowels"
+// afaik in Pig Latin output for "my" should be "ymay" and not "myay", but who knows
+console.log(translatePigLatin("my"));
--- Spinal Tap Case.js
+++ Spinal Tap Case.js
@@ -1,7 +1,7 @@
 //Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
 function spinalCase(str) {
-    return str2 = str.replace(/([A-Z])/g, ' $1').split(/\W+|_/g).filter(x => x !== "").join("-").toLowerCase();
+    return str.replace(/([A-Z])/g, ' $1').split(/\W+|_/g).filter(x => x !== "").join("-").toLowerCase();
 }
 
 console.log(spinalCase('The_Andy_Griffith_show'));
