26e9e9cdbf6e8eed572885125318530b9c42b731
Cube <jsochan@protonmail.com>
2019-06-06 11:01:42 +0200

    solution

--- Search and Replace.js
+++ Search and Replace.js
@@ -5,7 +5,9 @@
 //Note: Preserve the case of the first character in the original word.
 
 function myReplace(str, before, after) {
-    return str.replace(before, after);
+  if (before.search(/^[A-Z]/) == 0) {
+    return str.replace(before, (after.charAt(0).toUpperCase() + after.slice(1)));
   }
-  
-  console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
+  return str.replace(before, after);
+}
+console.log(myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped"));
