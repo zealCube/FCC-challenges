990d7be8adcd7f09462ca077ee7b49ed14f0e2ef
Cube <jsochan@protonmail.com>
2019-06-06 11:07:03 +0200

    init

---
+++ DNA Pairing.js
@@ -0,0 +1,10 @@
+//Take each character, get its pair, and return the results as a 2d array.
+//Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+//Return the provided character as the first element in each array.
+//for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]
+
+function pairElement(str) {
+    return str;
+  }
+  
+  pairElement("GCG");
