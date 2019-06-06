34ec8f16c2071313107e2cfdcbb8bb65901a316d
Cube <jsochan@protonmail.com>
2019-06-06 19:37:05 +0200

    init

---
+++ Sorted Union.js
@@ -0,0 +1,10 @@
+//function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+//all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+//The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+
+function uniteUnique(arr) {
+    return arr;
+  }
+  
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
