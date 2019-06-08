41174f12a874e5ffa57431be90aafc90965be3f7
Cube <jsochan@protonmail.com>
2019-06-08 22:04:25 +0200

    init

---
+++ Sum All Primes.js
@@ -0,0 +1,9 @@
+//Sum all the prime numbers up to and including the provided number.
+//The provided number may not be a prime.
+
+function sumPrimes(num) {
+    return num;
+  }
+  
+  
+  sumPrimes(10);
