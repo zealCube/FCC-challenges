71de0a4f5f41868f1509edeb0a62b907cde377bc
Cube <jsochan@protonmail.com>
2019-06-08 22:01:22 +0200

    solution

--- Sum All Odd Fibonacci Numbers.js
+++ Sum All Odd Fibonacci Numbers.js
@@ -2,7 +2,11 @@
 //sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
 
 function sumFibs(num) {
-    return num;
-  }
-  
-  sumFibs(4);
+    let sol = [0,1];
+    for (let i = 2; i <= num; i++) {
+        sol.push(sol[i - 1] + sol[i - 2]);
+    }
+    return sol.filter( x => (x%2 !== 0 && x <= num)).reduce((x,y) => x+y);
+}
+
+console.log(sumFibs(75024));
