fd071e7d1d1006d870922021d09a54e32ab554de
Cube <jsochan@protonmail.com>
2019-06-08 18:48:51 +0200

    solution(s)

--- Convert HTML Entities.js
+++ Convert HTML Entities.js
@@ -2,8 +2,20 @@
 //in a string to their corresponding HTML entities.
 
 function convertHTML(str) {
-    // &colon;&rpar;
+    var encodedStr = str.replace(/[\u00A0-\u9999<>\&]/g, function (i) {
+        return '&#' + i.charCodeAt(0) + ';';
+    });
+    return encodedStr;
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
+
+//Solution above is returning HTML entities "numbers", but FCC challenge is looking for HTML tags!
+// although it is not specified, solution that passes FCC challenge below
+
+function convertHTML(str) {
+    str = str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, "&apos;");
     return str;
-  }
-  
-  convertHTML("Dolce & Gabbana");
+}
+
+convertHTML("Dolce & Gabbana");
