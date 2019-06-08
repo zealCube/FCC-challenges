bbab88ae088a45d07382c3115e5eb66a50399bd2
Cube <jsochan@protonmail.com>
2019-06-08 19:00:34 +0200

    init

---
+++ Sum All Odd Fibonacci Numbers.js
@@ -0,0 +1,8 @@
+//Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+//sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+
+function sumFibs(num) {
+    return num;
+  }
+  
+  sumFibs(4);
