1714069f44de52a33eb4e497335e1d423ab56ba7
Cube <jsochan@protonmail.com>
2019-06-09 10:23:56 +0200

    init

---
+++ Smallest Common Multiple.js
@@ -0,0 +1,9 @@
+//Find the smallest common multiple of the provided parameters that can be evenly divided by both, 
+//as well as by all sequential numbers in the range between these parameters.
+
+function smallestCommons(arr) {
+    return arr;
+  }
+  
+  
+  smallestCommons([1,5]);
