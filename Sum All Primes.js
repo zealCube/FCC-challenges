ad45681a9de95934fbdf067ae283a35a88514e07
Cube <jsochan@protonmail.com>
2019-06-09 10:20:19 +0200

    solution

--- Sum All Primes.js
+++ Sum All Primes.js
@@ -2,8 +2,34 @@
 //The provided number may not be a prime.
 
 function sumPrimes(num) {
-    return num;
-  }
-  
-  
-  sumPrimes(10);
+    var res = 0;
+
+    // Function to get the primes up to max in an array
+    function getPrimes(max) {
+        var sieve = [], i, j, primes = [];
+        for (i = 2; i <= max; ++i) {
+            if (!sieve[i]) {
+                // i has not been marked -- it is prime
+                primes.push(i);
+                for (j = i << 1; j <= max; j += i) {
+                    sieve[j] = true;
+                }
+            }
+        }
+
+        return primes;
+    }
+
+    // Add the primes
+    var primes = getPrimes(num);
+    for (var p = 0; p < primes.length; p++) {
+        res += primes[p];
+    }
+
+    return res;
+}
+
+
+console.log(sumPrimes(20));
+
+//solution stole from the net
