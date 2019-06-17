e120d154124e89d96c76175ea7b12c695393bf75
Cube <jsochan@protonmail.com>
2019-06-17 22:50:19 +0200

    sol+init

---
+++ Arguments Optional.js
@@ -0,0 +1,7 @@
+/* see corresponding FCC challenge*/
+
+function addTogether() {
+    return false;
+  }
+  
+  addTogether(2,3);
---
+++ Everything Be True.js
@@ -0,0 +1,8 @@
+/* Check if the predicate (second argument) is truthy on all elements of a collection (first argument).*/
+
+function truthCheck(collection, pre) {
+    
+    return collection.every(x => x[pre]);
+  }
+  
+  truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
