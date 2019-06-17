c6723c8f5b75cdcb7e66606a12d4f4a1b1f0060d
Cube <jsochan@protonmail.com>
2019-06-17 23:03:36 +0200

    progress

--- Arguments Optional.js
+++ Arguments Optional.js
@@ -1,7 +1,13 @@
 /* see corresponding FCC challenge*/
 
 function addTogether() {
-    return false;
-  }
-  
-  addTogether(2,3);
+    if (arguments.length == 2 && typeof(arguments) == Number) {
+        return arguments[0] + arguments[1];
+    } /*else {
+        var sumTwoAnd = addTogether(2);
+    }*/
+
+    return undefined;
+}
+
+console.log(addTogether(2, "3"));
