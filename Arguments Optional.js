58453573b32dc19b4660841e29a35ac8d1db28a1
Cube <jsochan@protonmail.com>
2019-06-23 18:39:36 +0200

    solution

--- Arguments Optional.js
+++ Arguments Optional.js
@@ -1,13 +1,17 @@
 /* see corresponding FCC challenge*/
 
 function addTogether() {
-    if (arguments.length == 2 && typeof(arguments) == Number) {
-        return arguments[0] + arguments[1];
-    } /*else {
-        var sumTwoAnd = addTogether(2);
-    }*/
-
-    return undefined;
+    var arg = Array.from(arguments);
+    if (arg.some(n => typeof n !== "number") || arg.lenth > 2) {
+        return undefined;
+    }
+    if (arg.length === 2) {
+        return arg[0] + arg[1];
+    } else if (arg.length == 1) {
+        let a = arg[0];
+        return (b => (typeof b !== "number") ? undefined : (a + b));
+    }
 }
 
-console.log(addTogether(2, "3"));
+
+console.log(addTogether(4)(5));
