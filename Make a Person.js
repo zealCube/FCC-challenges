0804583384b5e1294af5fa608fff04f96f588f6f
Cube <jsochan@protonmail.com>
2019-06-23 21:15:51 +0200

    init

---
+++ Make a Person.js
@@ -0,0 +1,14 @@
+//Fill in the object constructor with the following methods below:
+//getFirstName() getLastName() getFullName() setFirstName(first) setLastName(last) setFullName(firstAndLast)
+
+
+var Person = function(firstAndLast) {
+    // Complete the method below and implement the others similarly
+    this.getFullName = function() {
+      return "";
+    };
+    return firstAndLast;
+  };
+  
+  var bob = new Person('Bob Ross');
+  bob.getFullName();
