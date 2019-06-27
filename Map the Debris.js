db8c4ce027d15f1020ad9f6a84c888cbb8421fa8
Cube <jsochan@protonmail.com>
2019-06-27 09:54:02 +0200

    init

---
+++ Map the Debris.js
@@ -0,0 +1,10 @@
+// Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+// more details on FCC
+
+function orbitalPeriod(arr) {
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+    return arr;
+  }
+  
+  orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
