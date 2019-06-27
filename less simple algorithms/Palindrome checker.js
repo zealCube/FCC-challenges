ac243850e49a419133033da660dc2a5ebd87bdec
Cube <jsochan@protonmail.com>
2019-06-27 12:34:10 +0200

    solution

--- less simple algorithms/Palindrome checker.js
+++ less simple algorithms/Palindrome checker.js
@@ -1,8 +1,10 @@
 //Return true if the given string is a palindrome. Otherwise, return false.
 
 function palindrome (str){
+    var str2 = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    var pal = str2.split("").reverse().join(""); //reverses the string and matches the original
+    return (pal === str2 ? true : false);
+};
 
-}
 
-
-console.log(palindrome(eye));
+console.log(palindrome("eYe;;"));
