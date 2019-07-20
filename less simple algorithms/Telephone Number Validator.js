9ac37ad556e67158064b7f97c7615664920f41ca
zealCube <jsochan@protonmail.com>
2019-07-20 11:11:16 +0200

    well

--- less simple algorithms/Telephone Number Validator.js
+++ less simple algorithms/Telephone Number Validator.js
@@ -3,22 +3,20 @@
 function telephoneCheck(str) {
     if (str.match(/[0-9]/g).length > 11 | str.match(/[0-9]/g).length < 10) {
         return false;
-    } else if (str.match(/[0-9]/g).length = 11) {
+    } else if (str.match(/[0-9]/g).length == 11) {
         if (str.charAt(0) != 1) {
             return false;
-        } else {
+        } else if (str.match(/[()]/g).length % 2 == 0 && str.charAt(0) != "(") {
+                return true;
+            }
+            return false;
 
-
-
+    } else if (str.match(/[0-9]/g).length == 10) {
+        if (str.match(/[()]/g).length % 2 == 0 && str.charAt(0) != "(") {
+            return true;
         }
-
-    } else if (str.match(/[0-9]/g).length = 10) {
-
-
-
-    } else {
         return false;
     }
 }
 
-console.log(telephoneCheck("5-555-5555"));
+console.log(telephoneCheck("1 555 555-5555"));
