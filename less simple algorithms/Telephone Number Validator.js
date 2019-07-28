da600e6ce6c5ec174f0ec14f3569146be49cd519
zealCube <jsochan@protonmail.com>
2019-07-28 12:48:23 +0200

    well

--- less simple algorithms/Telephone Number Validator.js
+++ less simple algorithms/Telephone Number Validator.js
@@ -1,22 +1,7 @@
 //Return true if the passed string looks like a valid US phone number.
 
 function telephoneCheck(str) {
-    if (str.match(/[0-9]/g).length > 11 | str.match(/[0-9]/g).length < 10) {
-        return false;
-    } else if (str.match(/[0-9]/g).length == 11) {
-        if (str.charAt(0) != 1) {
-            return false;
-        } else if (str.match(/[()]/g).length % 2 == 0 && str.charAt(0) != "(") {
-                return true;
-            }
-            return false;
-
-    } else if (str.match(/[0-9]/g).length == 10) {
-        if (str.match(/[()]/g).length % 2 == 0 && str.charAt(0) != "(") {
-            return true;
-        }
-        return false;
-    }
+    return (/^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/).test(str);
 }
 
 console.log(telephoneCheck("1 555 555-5555"));
